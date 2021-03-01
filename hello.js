86925b3b4578350fe7d0567072cbd7d98b0d42b9
JuanPabloBettolli <73622205+JuanPabloBettolli@users.noreply.github.com>
2021-03-01 19:37:02 -0300

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,21 @@
+//console.log("Hello WORLD!!!")
+//console.time()
+
+//console.log ('Log Here') 
+
+//console.info('info Here')
+
+//console.table(["Audi", "volvo", "Ford", "BMW"])
+
+/*console.table([{name: 'Juan', Age: 41}, {name: 'Pedro', Age: 7} ,
+{name: 'Maria', Age: 40}, {name: 'Bruno', Age: 5}]) */
+
+//console.info('Another line')
+
+//console.timeEnd()
+
+//console.count(6);
+
+console.log ("Juan")
+console.log ("Pablo")
+console.log("Bettolli")
