250b068bc86b9db2e621e85b356c50d6e981bd85
JuanPabloBettolli <73622205+JuanPabloBettolli@users.noreply.github.com>
2021-03-01 21:59:23 -0300

    Add files via upload

--- 1-variables.js
+++ 1-variables.js
@@ -1,4 +1,32 @@
-console.log("Hi")
+//aca console log
+//console.log("Hi")
 
-/**
- */
+
+//console.log("Hi")
+
+//console.info("info")
+
+//console.table(['one','two','three'])
+//console.count()
+//console.count()
+
+//VARIABLES
+
+//let numero = 10
+//console.log(10)
+//console.log(numero)
+
+/* Concatena palabras
+const myFirstName = 'JUAN'
+const mySecondName = 'Pablo'
+const myLastName = 'Bettolli'
+
+console.log(myFirstName, mySecondName, myLastName)*/
+
+//console.log (12+2)
+
+// let user = "Juan Pablo", age ='31', message = "Hola"
+
+
+
+
